be3dd1edf36d5ef12deeeacf28925ad943074c6f
anthoniusmonk <anthoniusmonk@gmail.com>
2024-12-05 10:54:11 -0700

    final commit

--- db/migrate/20241205161102_add_complete_to_tasks.rb
+++ db/migrate/20241205161102_add_complete_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddCompleteToTasks < ActiveRecord::Migration[6.1]
   def change
-    add_column :tasks, :complete, :boolean
+    add_column :tasks, :complete, :boolean, default: false
   end
 end
